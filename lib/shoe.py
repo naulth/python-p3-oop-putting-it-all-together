12fbcb772cd6a307bf0f15678b193022f0574cfe
Hayden Nault <naulth@gmail.com>
2023-03-15 17:10:15 -0400

    passing all tests

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
+import ipdb
 
 class Shoe:
     
     def __init__(self, brand):
         self.brand = brand
+        self.size = 0
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"    
+
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, size):
+        if (type(size) in (int,)):
+            self._size = size
+        
+        else:
+            print("size must be an integer")
+
+    size = property(get_size, set_size,)
